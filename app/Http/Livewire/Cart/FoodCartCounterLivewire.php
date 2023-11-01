88bd433cb7b7edec1a29a644b261cadfbe6413a7
DJ-Programmer-Micho <michelshabo1@gmail.com>
2023-11-01 14:32:56 +0300

    fix usecase linux

--- app/Http/Livewire/Cart/FoodCartCounterLivewire.php
+++ app/Http/Livewire/Cart/FoodCartCounterLivewire.php
@@ -1,6 +1,6 @@
 <?php
  
-namespace App\Http\Livewire\cart;
+namespace App\Http\Livewire\Cart;
  
 
 use App\Models\Food;
--- app/Http/Livewire/Cart/OfferCartLivewire.php
+++ app/Http/Livewire/Cart/OfferCartLivewire.php
@@ -1,6 +1,6 @@
 <?php
  
-namespace App\Http\Livewire\cart;
+namespace App\Http\Livewire\Cart;
  
 use App\Models\Offer;
 use Livewire\Component;
