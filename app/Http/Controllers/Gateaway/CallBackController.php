89cee13aab82c800449d88985be04d0ee66e0360
DJ-Programmer-Micho <michelshabo1@gmail.com>
2024-01-21 13:56:18 +0300

    log update

--- app/Http/Controllers/Gateaway/CallBackController.php
+++ app/Http/Controllers/Gateaway/CallBackController.php
@@ -55,7 +55,9 @@ class CallBackController extends Controller
         // ];
 
     public function areebaCallBack(){
-        dd(request()->all());
+        Log::info('Received Areeba Callback', ['data' => request()->all()]);
+
+        // dd(request()->all());
         $data = request()->all();
         $Transaction = Transaction::findOrFail($data['merchantTransactionId']);
         $plan = Plan::find($Transaction->plan_id);
